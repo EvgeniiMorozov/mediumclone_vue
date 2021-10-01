2f46d28991c765296fbdddb7ea7f06085d42a49b
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-01 15:27:20 +0500

    lesson 19 create feed

---
+++ src/components/Feed.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Feed !!! {{ apiUrl }}</div>
+</template>
+
+<script>
+import {actionsTypes} from '@/store/modules/feed'
+
+export default {
+  name: 'McvFeed',
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    console.log('init feed')
+    this.$store.dispatch(actionsTypes.getFeed, {apiUrl: this.apiUrl})
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -77,7 +77,7 @@ export default {
           password: this.password
         })
         .then(() => {
-          this.$router.push({name: 'home'})
+          this.$router.push({name: 'globalFeed'})
         })
     }
   }
