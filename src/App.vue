4d05316fc721c32cb1b68197a7a7b6a2b0383cc7
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-01 14:45:27 +0500

    lesson 18 current user

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
 
 <script>
 import McvTopbar from '@/components/TopBar'
+import {actionsTypes} from '@/store/modules/auth'
 
 export default {
   name: 'McvApp',
   components: {
     McvTopbar
+  },
+  mounted() {
+    console.log('hello app')
+    this.$store.dispatch(actionsTypes.getCurrentUser)
   }
 }
 </script>
