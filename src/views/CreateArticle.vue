5673c8399e4a762f2f2f1afab1097456014e43df
EvgeniiMorozov <emorozov1101@gmail.com>
2022-01-12 21:02:11 +0500

    lesson 30 - half

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Create Article
+    <mcv-article-form
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @articleSubmitt="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvCreateArticle',
+  data() {
+    return {
+      initialValues: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  methods: {
+    onSubmit(data) {
+      console.log('onSubmit in createArticle', data)
+    }
+  }
+}
+</script>
